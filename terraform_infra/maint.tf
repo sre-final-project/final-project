e8ac75387421592700c2d5a28e66b7d5ebd4583f
1mmka <erhan.sapabek@gmail.com>
2026-05-18 23:51:30 +0500

    Add full CI/CD pipeline with Ansible deploy

--- terraform_infra/maint.tf
+++ terraform_infra/maint.tf
@@ -131,7 +131,7 @@ data "aws_availability_zones" "available" {
 
 resource "aws_instance" "ecommerce" {
   ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ecommerce.id]
   key_name               = aws_key_pair.deployer.key_name
--- terraform_infra/terraform.tfvars
+++ terraform_infra/terraform.tfvars
@@ -1,5 +1,5 @@
-aws_region     = "us-east-1"
-instance_type  = "t2.micro"
+aws_region     = "eu-north-1"
+instance_type  = "t3.micro"
 vpc_cidr       = "10.0.0.0/16"
 subnet_cidr    = "10.0.1.0/24"
 environment    = "production"
